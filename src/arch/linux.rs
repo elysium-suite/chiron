c823cfb9cdf1a215b720ac42ca139f6cae5c286e
Rohan Juneja <rohanj2006@gmail.com>
2021-01-28 15:36:05 -0800

    add nightly to toolchain and add documentation

--- src/arch/linux.rs
+++ src/arch/linux.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use procfs::process::Process;
 
+/// Check if the process is being traced by reading /proc/self/status for tracerpid
 pub fn check_trace() -> Result<bool> {
 	let status = Process::myself()?.status()?;
 	Ok(status.tracerpid != 0)
