e698ab334aeac034cc1c2d70a25de026e2509a48
safinsingh <safin.singh@gmail.com>
2021-01-29 13:43:56 -0800

    ci: build correctly

--- src/arch/linux.rs
+++ src/arch/linux.rs
@@ -1,8 +1,8 @@
 use anyhow::Result;
 use procfs::process::Process;
 
-/// Check if the process is being traced by reading /proc/self/status for
-/// tracerpid
+/// Check if the process is being traced by checking `/proc/self/status` for
+/// `tracerpid`
 pub fn check_trace() -> Result<bool> {
 	let status = Process::myself()?.status()?;
 	Ok(status.tracerpid != 0)
