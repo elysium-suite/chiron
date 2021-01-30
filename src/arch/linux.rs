07def1de11b0401bc9d9e6c8932d883178d3d8f4
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2021-01-30 14:31:25 -0800

    create linux package installed check

--- src/arch/linux.rs
+++ src/arch/linux.rs
@@ -1,9 +1,19 @@
+use crate::checks::ScoreableCheck;
+use crate::config::PackageInstalled;
 use anyhow::Result;
 use procfs::process::Process;
+use apt_pkg_native::Cache;
 
 /// Check if the process is being traced by checking `/proc/self/status` for
 /// `tracerpid`
 pub fn check_trace() -> Result<bool> {
 	let status = Process::myself()?.status()?;
 	Ok(status.tracerpid != 0)
+}
+
+#[typetag::serde]
+impl ScoreableCheck for PackageInstalled {
+	fn score(&self) -> Result<bool> {
+		Ok(!matches!(Cache::get_singleton().find_by_name(&self.package).next(), None))
+	}
 }
