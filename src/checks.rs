cbce9070414b22dba25f350b65ce0501a0568918
raad1masum <piraadwan@gmail.com>
2021-01-30 18:50:52 -0800

    formatting :100:

--- src/checks.rs
+++ src/checks.rs
@@ -33,5 +33,7 @@ impl ScoreableCheck for PackageInstalled {
 
 #[typetag::serde]
 impl ScoreableCheck for FilePermissions {
-	fn score(&self) -> Result<bool> { file_permissions(&self.file, &self.perms) }
+	fn score(&self) -> Result<bool> {
+		file_permissions(&self.file, &self.perms)
+	}
 }
